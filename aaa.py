727f3f111145dd0c4606a4b42b47d5ccb7617a82
gitGut01 <sile@computas.com>
2024-10-15 14:44:51 +0200

    Fix line issue

--- aaa.py
+++ aaa.py
@@ -17,25 +17,52 @@ def get_skeleton(mask):
 
     return skeleton
 
-def get_most_common_value(group, axis_key):
-    """Get the most common value for the given axis in a group."""
-    axis_values = [item[axis_key] for item in group]
-    most_common_value = Counter(axis_values).most_common(1)[0][0]
-    return most_common_value
 
-def update_group_axis_values(group, axis_key, value):
-    """Update the axis values of all entries in the group to the given value."""
-    for entry in group:
-        entry[axis_key] = value
+def find_starting_point(binary_mask):
+    # Find most top left point
+    y, x = np.where(binary_mask == 255)
+    coordinates = sorted(zip(x, y), key=lambda coord: (coord[0], coord[1]))
+    return coordinates[0]
 
-def close_and_reset_group(groups, current_group, axis_key):
-    """Close the current group, update axis values, and add it to groups."""
-    most_common_value = get_most_common_value(current_group, axis_key)
-    update_group_axis_values(current_group, axis_key, most_common_value)
-    groups.append(current_group)
-    return most_common_value  # Return the most common value for the next group's initialization
 
-def process_groups(my_list, max_consecutive_increase_count=2, axis='y'):
+def get_kp_first_hit_x(binary_mask, steps=10):
+    height, width = binary_mask.shape
+    starting_point = find_starting_point(binary_mask)
+
+    kps = []
+
+    for x in range(starting_point[0], width-1, steps):
+        for y in range(0, height-1):
+            if binary_mask[y, x] == 255:
+                kps.append({
+                    'x': x,
+                    'y': y
+                })
+                break
+
+    return kps
+
+
+def get_kp_first_hit_y(binary_mask, steps=10):
+    height, width = binary_mask.shape
+    starting_point = find_starting_point(binary_mask)
+
+    kps = []
+
+    for y in range(starting_point[1], width, steps):
+        for x in range(width-1, 0, -1):
+            if binary_mask[y, x] == 255:
+                kps.append({
+                    'x': x,
+                    'y': y
+                })
+                break
+
+    return kps
+                
+
+
+def process_groups(my_list, max_consecutive_increase_count=4, axis='y'):
     """
     Process groups of entries in `my_list` based on consecutive increases in the x or y values.
 
@@ -74,11 +101,23 @@ def process_groups(my_list, max_consecutive_increase_count=2, axis='y'):
 
         # If there are 'max_consecutive_increase_count' consecutive increases, close the current group
         if consecutive_increase_count >= max_consecutive_increase_count:
-            prev_value = close_and_reset_group(groups, current_group, axis_key)
-            # Start a new group with the current item
+            # Get the most common value of the axis in the current group
+            axis_values = [item[axis_key] for item in current_group]
+            most_common_value = Counter(axis_values).most_common(1)[0][0]
+
+            # Set all axis values in the current group to the most common value
+            for entry in current_group:
+                entry[axis_key] = most_common_value
+
+            # Add the group to the list of groups
+            groups.append(current_group)
+
 
             start_pos = (current_group[0]['x'], current_group[0]['y'])
+
+            # Start a new group with the current item
             current_group = [entry]
+            prev_value = entry[axis_key]  # Update the previous value for the new group
             consecutive_increase_count = 0
 
             groups_info.append({
@@ -89,12 +128,18 @@ def process_groups(my_list, max_consecutive_increase_count=2, axis='y'):
             })
 
         # Update the previous value to the most common value in the current group
-        prev_value = get_most_common_value(current_group, axis_key)
+        prev_value = Counter([item[axis_key] for item in current_group]).most_common(1)[0][0]
 
     # Process the last group
     if current_group:
-        close_and_reset_group(groups, current_group, axis_key)
+        axis_values = [item[axis_key] for item in current_group]
+        most_common_value = Counter(axis_values).most_common(1)[0][0]
+        for entry in current_group:
+            entry[axis_key] = most_common_value
 
+        groups.append(current_group)
+
+        start_pos = (current_group[0]['x'], current_group[0]['y'])
         groups_info.append({
             'group_id': len(groups),
             'start_pos': start_pos,
@@ -108,15 +153,18 @@ def process_groups(my_list, max_consecutive_increase_count=2, axis='y'):
 img_path = "sample_result_mask_3.png"
 # img_path = "inst_mask_3.png"
 binary_mask = load_binary_mask(img_path)
-new_image = cv2.cvtColor(binary_mask, cv2.COLOR_GRAY2BGR)
 
+new_image = cv2.cvtColor(binary_mask, cv2.COLOR_GRAY2BGR)
 binary_mask = get_skeleton(binary_mask)
+
+cv2.imwrite("skeleton.png", binary_mask)
+
+
 
 
 
 
 x_range = line_utils.get_xrange(binary_mask)
-print(x_range)
 
 
 
@@ -125,11 +173,16 @@ print(x_range)
 
 
 
-key_points_x = line_utils.get_kp(binary_mask, interval=1, x_range=None, get_num_lines=False, get_center=True)
+# key_points_x = line_utils.get_kp(binary_mask, interval=1, x_range=None, get_num_lines=False, get_center=True)
+key_points_x = get_kp_first_hit_x(binary_mask, steps=1)
 new_image = line_utils.draw_kps(new_image, key_points_x, color=(200,127,127))
 
-key_points_y = line_utils.get_kp_y(binary_mask, interval=1, y_range=None, get_num_lines=False, get_center=True)
-new_image = line_utils.draw_kps(new_image, key_points_y, color=(0,127,255))
+
+key_points_y = get_kp_first_hit_y(binary_mask, steps=1)
+new_image = line_utils.draw_kps(new_image, key_points_y, color=(127,127,200))
+
+# key_points_y = line_utils.get_kp_y(binary_mask, interval=1, y_range=None, get_num_lines=False, get_center=True)
+# new_image = line_utils.draw_kps(new_image, key_points_y, color=(0,127,255))
 
 
 
@@ -153,8 +206,9 @@ def draw_lines_y(img, group_dict):
 
 
 key_points_x, group_x_dict = process_groups(key_points_x, axis='y')
-# new_image = line_utils.draw_kps(new_image, key_points_x, color=(0,255,0))
 new_image = draw_lines_x(new_image, group_x_dict)
+# new_image = line_utils.draw_kps(new_image, key_points_x, color=(0,255,0))
+
 
 
 
@@ -162,7 +216,12 @@ key_points_y, group_y_dict = process_groups(key_points_y, axis='x')
 new_image = draw_lines_y(new_image, group_y_dict)
 # new_image = line_utils.draw_kps(new_image, key_points_y, color=(0,0,255))
 
-print(group_x_dict)
+
+
+
+def get_crossings(group_x_dict, group_y_dict):
+    pass
+
 
 
 
